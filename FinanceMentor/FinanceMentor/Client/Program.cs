45ce2705694223668bbdd3fe52f0aeb6a356527a
Marcelo Krol <marcelokrol@yahoo.com.br>
2021-12-04 15:19:17 -0300

    #7

--- FinanceMentor/FinanceMentor/Client/Program.cs
+++ FinanceMentor/FinanceMentor/Client/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using FinanceMentor.Client.Services;
 
 namespace FinanceMentor.Client
 {
@@ -18,6 +19,7 @@ namespace FinanceMentor.Client
          builder.RootComponents.Add<App>("#app");
 
          builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+         builder.Services.AddScoped<IDataservice.IDataService, DataService>();
 
          await builder.Build().RunAsync();
       }
